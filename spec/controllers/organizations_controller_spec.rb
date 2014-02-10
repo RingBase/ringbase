c3c9f1e11c6f27f7184e302ce35f13ddc7d784cd
Andrew Berls <andrew.berls@gmail.com>
2014-02-09 16:03:51 -0800

    Remove auto-generated failing test cases

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe OrganizationsController do
-
-  describe "GET 'dashboard'" do
-    it "returns http success" do
-      get 'dashboard'
-      response.should be_success
-    end
-  end
-
 end
--- spec/models/placeholder_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe "Placeholder" do
-  it "works" do
-    true.should == true
-  end
-end
