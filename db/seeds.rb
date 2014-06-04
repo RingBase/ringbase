0179679e39662228a53c59ef0db7d8f351a737ad
Petesta <Petesta@live.com>
2014-06-04 14:40:20 -0700

    Montez is part of RingBase. :grin:

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.delivery_method = :test
 #admin_user = User.create!(full_name: "Admin User",  email: "admin@admin.com",  password: "admin", password_confirmation: "admin")
 
 
-org = Organization.create!(name: "Best Travel Agency")
+org = Organization.create!(name: "TelAmeriCorp")
 
 # CONTROLLED_NATIONAL_NUMBERS = [
 #   '8053945120',
@@ -22,7 +22,7 @@ org.campaigns.create!(title: org.name, pilot_number: "8053945120")
 
 
 [
- { full_name: 'John Smith', email: 'john@best.com', phone_number: '7073227256' },
+ { full_name: 'Montez Walker', email: 'montez.ringbase@gmail.com', phone_number: '7073227256' },
  { full_name: 'Andrew Twirls', email: 'andrew.berls@gmail.com', phone_number: '7073227256' },
  { full_name: 'Nivedh Mohinder', email: 'nivedh.mudaliar@gmail.com', phone_number: '4083489187' },
  { full_name: 'Alex Timber', email: 'awood314@gmail.com', phone_number: '8054058403' },
